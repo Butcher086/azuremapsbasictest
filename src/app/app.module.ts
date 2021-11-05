1b9661bbb68811c0a5da458c83683b52088104fb
Ruben Asensio <ruben.asensio@hitachivantara.com>
2021-11-05 12:22:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import * as atlas from 'azure-maps-control';
+import { AzureMapsModule } from 'ng-azure-maps';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AzureMapsModule.forRoot({
+      authOptions: {
+        authType: atlas.AuthenticationType.subscriptionKey,
+        subscriptionKey: 'EtJQbIoVG-x2GXRqs4g_WbiTj5fo_1q7IUG-p731m_Y'
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
